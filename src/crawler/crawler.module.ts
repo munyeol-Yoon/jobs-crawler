5c75eaad69c93866331786357d661fb053301ff0
muyeon <gmakin36@gmail.com>
2024-10-17 20:20:18 +0900

    feat: connected DB

--- src/crawler/crawler.module.ts
+++ src/crawler/crawler.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
-import { CrawlerService } from './crawler.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CrawlerController } from './crawler.controller';
+import { CrawlerService } from './crawler.service';
+import { Job } from './entity/job.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Job])],
   controllers: [CrawlerController],
   providers: [CrawlerService],
 })
